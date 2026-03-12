ac19b4459fc602042b945aef7b7dd4379599e613
Constantine <foofightermail@gmail.com>
2026-03-13 01:46:01 +0300

    added picocli dependency and updated App per picocli template

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+        name = "app",
+        description = "Test CLI application",
+        mixinStandardHelpOptions = true
+)
+public class App implements Runnable {
+
+
+        @Override
+        public void run() {
+            System.out.println("Hello World!");
+        }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute(args);
     }
 }
