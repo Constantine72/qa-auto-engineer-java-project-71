00e5cb195cdd3b5cc52197b277a1f37f3832c3fc
Constantine <foofightermail@gmail.com>
2026-03-15 23:15:48 +0300

    added DifferTest.java and updated Makefile

---
+++ app/src/test/java/DifferTest.java
@@ -0,0 +1,34 @@
+import hexlet.code.Differ;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DifferTest {
+
+    @Test
+    public void testFlatJsonComparison() throws Exception {
+
+        String path1 = "src/test/resources/file1.json";
+        String path2 = "src/test/resources/file2.json";
+
+        String expected =
+                " - follow: false\n"
+                        +
+                "   host: hexlet.io\n"
+                        +
+                " - proxy: 123.234.53.22\n"
+                        +
+                " - timeout: 50\n"
+                        +
+                " + timeout: 20\n"
+                        +
+                " + verbose: true\n"
+                        +
+                "}";
+
+        String actual = Differ.generate(path1, path2);
+
+        assertEquals(expected, actual);
+    }
+}
+
