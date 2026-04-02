81bd3324f0edb68d57d661bca877682014cd7f52
Constantine <foofightermail@gmail.com>
2026-04-02 22:29:52 +0300

    removed comments from App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,8 +5,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+
 
 import java.util.concurrent.Callable;
 
@@ -14,7 +13,7 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer> {
 
-//    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
 
 
     @Parameters(index = "0", paramLabel = "filePath1", description = "path to first file")
@@ -30,7 +29,7 @@ class App implements Callable<Integer> {
     public Integer call() throws Exception {
 
         var result = Differ.generate(filePath1, filePath2, format);
-        //NOSONAR
+
         System.out.println(result);
 
         return 0;
