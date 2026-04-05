030a98a15590f5975e275b0208ffca806ca807b8
Constantine <foofightermail@gmail.com>
2026-04-05 17:26:11 +0300

    added StylishTest

---
+++ app/src/test/java/StylishTest.java
@@ -0,0 +1,17 @@
+import hexlet.code.formatters.JSON;
+import hexlet.code.formatters.Stylish;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StylishTest {
+    @Test
+    void testConstructorIsPrivate() throws Exception {
+        java.lang.reflect.Constructor<Stylish> constructor = Stylish.class.getDeclaredConstructor();
+        assertTrue(java.lang.reflect.Modifier.isPrivate(constructor.getModifiers()),
+                "Constructor must be private");
+        constructor.setAccessible(true);
+        assertThrows(java.lang.reflect.InvocationTargetException.class, constructor::newInstance);
+    }
+}
