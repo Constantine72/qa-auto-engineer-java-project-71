8694ae629a6c7be0a7c6a7d474a68e7e8cbef98c
Constantine <foofightermail@gmail.com>
2026-04-05 19:10:32 +0300

    updated PlainTest

--- app/src/test/java/PlainTest.java
+++ app/src/test/java/PlainTest.java
@@ -1,6 +1,8 @@
+import hexlet.code.Differ;
 import hexlet.code.formatters.Plain;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,5 +15,14 @@ public class PlainTest {
                 "Constructor must be private");
         constructor.setAccessible(true);
         assertThrows(java.lang.reflect.InvocationTargetException.class, constructor::newInstance);
+    }
+    @Test
+    void testPlainRecursive() throws Exception {
+        String filePath1 = "src/test/resources/fileNestedTest1.json";
+        String filePath2 = "src/test/resources/fileNestedTest2.json";
+
+        String result = Differ.generate(filePath1, filePath2, "plain");
+
+        assertThat(result.contains("Property 'nestedKey.value1' was updated"));
     }
 }
